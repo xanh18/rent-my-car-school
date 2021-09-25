e4381cbf1a6359a5bee42e93038aef430491668a
Outis073 <72567157+Outis073@users.noreply.github.com>
2021-09-25 12:08:18 +0200

    Initial commit

---
+++ src/main/java/nl/rentmycar/Rent/My/Car/RentMyCarApplication.java
@@ -0,0 +1,13 @@
+package nl.rentmycar.Rent.My.Car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentMyCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentMyCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/rentmycar/Rent/My/Car/RentMyCarApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rentmycar.Rent.My.Car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentMyCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
