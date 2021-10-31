2954b401182be4c743646795825623a64ecfb398
ccmavans <72778596+ccmavans@users.noreply.github.com>
2021-10-31 19:48:48 +0100

    Removed double import

--- src/main/java/nl/rentmycar/RentMyCarApplication.java
+++ src/main/java/nl/rentmycar/RentMyCarApplication.java
@@ -3,7 +3,6 @@ package nl.rentmycar;
 
 import nl.rentmycar.Car.Car;
 import nl.rentmycar.Trip.Trip;
-import nl.rentmycar.User.UserRepository;
 import nl.rentmycar.User.User;
 import nl.rentmycar.User.UserRepository;
 import org.springframework.boot.CommandLineRunner;
